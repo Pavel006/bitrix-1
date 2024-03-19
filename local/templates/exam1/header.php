f65c96f9b656bacd974a5e1e9f5ffe6faf8756ea
Pavel Sevrugin <pavel473945814@gmail.com>
2024-03-19 15:53:16 +0300

    Добавлен раздел с динамической информацией 'Отзывы'

--- local/templates/exam1/header.php
+++ local/templates/exam1/header.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
 <html lang="ru">
 
-<head>
-    <title>Внутренняя</title>
+<head>    <title><?=$APPLICATION->ShowTitle();?></title>
+
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta charset="utf-8" />
-    <meta name="keywords" content="">
-    <meta name="description" content="">
-    <meta name="author" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH;?>/css/reset.css" />
     <link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH;?>/css/style.css" />
